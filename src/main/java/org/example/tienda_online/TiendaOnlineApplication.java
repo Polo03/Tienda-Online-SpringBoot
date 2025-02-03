1b1318bf97a099e586d63780c8d663fa8b01f240
Adrian Polo <adrianpoloper@gmail.com>
2025-02-03 09:22:54 +0100

    subida principal

--- src/main/java/org/example/tienda_online/TiendaOnlineApplication.java
+++ src/main/java/org/example/tienda_online/TiendaOnlineApplication.java
@@ -2,8 +2,12 @@ package org.example.tienda_online;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties
 public class TiendaOnlineApplication {
 
     public static void main(String[] args) {
