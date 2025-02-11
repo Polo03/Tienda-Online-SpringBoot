13aeb59dbb3ed696b6097e9cb5e320a3c5c89af4
Adrian Polo <adrianpoloper@gmail.com>
2025-02-11 11:54:37 +0100

    subida principal

--- src/main/java/org/example/tienda_online/Repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.tienda_online.Repository;
-
-import org.example.tienda_online.Dto.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-
-}
--- src/main/java/org/example/tienda_online/Repository/CompraRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.tienda_online.Repository;
-
-import org.example.tienda_online.Dto.Compra;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompraRepository extends JpaRepository<Compra, Integer> {
-}
