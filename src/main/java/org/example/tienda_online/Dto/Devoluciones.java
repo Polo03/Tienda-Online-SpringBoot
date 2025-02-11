92d4ade75b8e5a98ef0e92a050f93450c618e14f
Adrian Polo <adrianpoloper@gmail.com>
2025-02-11 13:38:38 +0100

    devoluciones

---
+++ src/main/java/org/example/tienda_online/Dto/Devoluciones.java
@@ -0,0 +1,46 @@
+package org.example.tienda_online.Dto;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "devoluciones")
+public class Devoluciones {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+}
---
+++ src/main/java/org/example/tienda_online/Repository/DevolucionesRepository.java
@@ -0,0 +1,7 @@
+package org.example.tienda_online.Repository;
+
+import org.example.tienda_online.Dto.Devoluciones;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DevolucionesRepository extends JpaRepository<Devoluciones, Integer> {
+}
