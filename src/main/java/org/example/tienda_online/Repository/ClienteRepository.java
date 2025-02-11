acddb8f14867ef6ded7c0bdd7fb87a310aa24a18
Adrian Polo <adrianpoloper@gmail.com>
2025-02-11 12:35:06 +0100

    commit repositorys

--- src/main/java/org/example/tienda_online/Repository/ClienteRepository.java
+++ src/main/java/org/example/tienda_online/Repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package org.example.tienda_online.Repository;
 
 import org.example.tienda_online.Dto.Cliente;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
 }
--- src/main/java/org/example/tienda_online/Repository/ProductoRepository.java
+++ src/main/java/org/example/tienda_online/Repository/ProductoRepository.java
@@ -2,6 +2,8 @@ package org.example.tienda_online.Repository;
 
 import org.example.tienda_online.Dto.Producto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductoRepository extends CrudRepository<Producto, Integer> {
 }
