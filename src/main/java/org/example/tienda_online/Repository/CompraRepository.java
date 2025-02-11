3446fa79bce9513e5180ece8e85708b95618c75a
Adrian Polo <adrianpoloper@gmail.com>
2025-02-11 12:42:02 +0100

    commit repositorys

--- src/main/java/org/example/tienda_online/Repository/CompraRepository.java
+++ src/main/java/org/example/tienda_online/Repository/CompraRepository.java
@@ -1,9 +1,10 @@
 package org.example.tienda_online.Repository;
 
 import org.example.tienda_online.Dto.Compra;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompraRepository extends CrudRepository<Compra, Integer> {
+public interface CompraRepository extends JpaRepository<Compra, Integer> {
 }
